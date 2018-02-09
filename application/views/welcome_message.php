610edec0163f3784b621b55092d9fe9868c0469c
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-02-09 13:38:37 -0800

    Added Bento Box and Menu

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,14 +1,100 @@
-<h1>Welcome to CodeIgniter!</h1>
+<div>
+  <h1>Create your BentoBox!</h1>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        <h2>BentoBox</h2>
+        <div class="row">
+          <div class="col-8 acc">
+            <h3>Main</h3>
+          </div>
+          <div class="col-4 acc">
+            <h3>Side</h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col acc">
+            <h3>Sushi</h3>
+          </div>
+          <div class="col acc">
+            <h3>Salad</h3>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="row">
+          <div class="col-12"><h2>Menu</h2></div>
 
-<div id="body">
-	<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+          <div class="col-6"><h3>Main</h3></div>
+          <div class="col-6">
+            <div class="btn-group dropright">
+              <button type="button" class="btn btn-secondary dropdown-toggle"
+              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Choose a Main
+              </button>
+              <div class="dropdown-menu">
+                <button id="chickenTeriyaki" class="dropdown-item" type="button">Chicken Teriyaki</button>
+                <button id="omuRice" class="dropdown-item" type="button">Omu Rice</button>
+                <button id="grilledMackarel" class="dropdown-item" type="button">Grilled Mackarel</button>
+                <button id="udon" class="dropdown-item" type="button">Udon</button>
+                <button id="unagi" class="dropdown-item" type="button">Unagi</button>
+              </div>
+            </div>
+          </div>
 
-	<p>If you would like to edit this page you'll find it located at:</p>
-	<code>application/views/welcome_message.php</code>
+          <div class="col-6"><h3>Side</h3></div>
+          <div class="col-6">
+            <div class="btn-group dropright">
+              <button type="button" class="btn btn-secondary dropdown-toggle"
+              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Choose a Side
+              </button>
+              <div class="dropdown-menu">
+                <button id="yamTempura" class="dropdown-item" type="button">Yam Tempura</button>
+                <button id="agedashiTofu" class="dropdown-item" type="button">Agedashi Tofu</button>
+                <button id="shripTempura" class="dropdown-item" type="button">Shirmp Tempura</button>
+                <button id="misoSoup" class="dropdown-item" type="button">Miso Soup</button>
+              </div>
+            </div>
+          </div>
 
-	<p>The corresponding controller for this page is found at:</p>
-	<code>application/controllers/Welcome.php</code>
+          <div class="col-6"><h3>Sushi</h3></div>
+          <div class="col-6">
+            <div class="btn-group dropright">
+              <button type="button" class="btn btn-secondary dropdown-toggle"
+              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Choose a Roll
+              </button>
+              <div class="dropdown-menu">
+                <button id="salmonRoll" class="dropdown-item" type="button">Salmon Roll</button>
+                <button id="tunaRoll" class="dropdown-item" type="button">Tuna Roll</button>
+                <button id="shripNigiri" class="dropdown-item" type="button">Shirmp Nigiri</button>
+                <button id="salmonSashimi" class="dropdown-item" type="button">Salmon Sashimi</button>
+                <button id="tunaSashimi" class="dropdown-item" type="button">Tuna Sashimi</button>
+              </div>
+            </div>
+          </div>
 
-	<p>If you are exploring CodeIgniter for the very first time, you should 
-		start by reading the <a href="http://www.codeigniter.com/user_guide/">User Guide</a>.</p>
+          <div class="col-6"><h3>Salad</h3></div>
+          <div class="col-6">
+            <div class="btn-group dropright">
+              <button type="button" class="btn btn-secondary dropdown-toggle"
+              data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Choose a Salad
+              </button>
+              <div class="dropdown-menu">
+                <button id="ebiSunomono" class="dropdown-item" type="button">Ebi Sunomono</button>
+                <button id="goma-ae" class="dropdown-item" type="button">Goma-ae</button>
+                <button id="potatoSalad" class="dropdown-item" type="button">Potato Salad</button>
+                <button id="japaneseSalad" class="dropdown-item" type="button">Japanese Salad</button>
+                <button id="japaneseSalad" class="dropdown-item" type="button">Seaweed Salad</button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="row"></div>
+      </div>
+    </div>
+  </div>
 </div>
