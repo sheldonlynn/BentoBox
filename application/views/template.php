6fc67f0215719acd28b1b80666184d4b9c331fab
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-02-11 15:29:35 -0800

    Merge branch 'develop' of https://github.com/4711Accessorize/BentoBox into feature-welcomeView
    # Conflicts:
    #application/views/template.php
    #public/assets/css/default.css

--- application/views/template.php
+++ application/views/template.php
@@ -20,7 +20,7 @@
         <div class="inner">
             <h3 class="masthead-brand">BentoBox</h3>
             <nav class="nav nav-masthead justify-content-center">
-                <a class="nav-link active" href="/">Home</a>
+                <a class="nav-link" href="/">Home</a>
                 <a class="nav-link" href="/catalog">Catalog</a>
             </nav>
         </div>
@@ -43,6 +43,9 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<!-- Homebrewed JS -->
+<script src="/assets/js/default.js"></script>
 </body>
 
 </html>
